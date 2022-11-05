d4034b1288ddcd5152848701f54109d01200a3c2
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-11-05 20:10:25 +0000

    Image CLI improvements

--- cli/image/image.py
+++ cli/image/image.py
@@ -4,6 +4,7 @@ import json
 import zipfile
 
 import typer
+import invoke
 import requests
 from tqdm import tqdm
 from natsort import natsorted
@@ -17,8 +18,14 @@ app = typer.Typer()
 def download(
         os_type: str = typer.Argument("debian11", help="OS or distro to download"),
         chunk_size: int = typer.Option(16, help="Download file chunk size"),
+        zfs_path: str = typer.Option("zroot/vm_encrypted", help="Set the ZFS dataset path"),
 ):
     """ Download a ready to deploy OS image """
+
+    if zfs_path[0] == "/" or zfs_path[-1] == "/":
+        print("Sorry, ZFS path can't start or end with '/'", file=sys.stderr)
+        sys.exit(1)
+
     json_image_url = "https://images.yari.pw/"
     json_images = requests.get(json_image_url)
     images_dict = json.loads(json_images.text)
@@ -45,9 +52,20 @@ def download(
     image_download_stream = requests.get(image_url, stream=True)
     image_size = int(image_download_stream.headers.get("content-length"))
 
+    image_end_location = "/" + zfs_path + "/template-" + os_type + "/"
+    if os.path.exists(image_end_location + "/disk0.img"):
+        print("Sorry, the image file already exists: " + "/" + zfs_path + "/template-" + os_type + "/disk0.img", file=sys.stderr)
+        sys.exit(1)
+    elif os.path.exists(image_end_location):
+        pass
+    else:
+        command = "zfs create " + zfs_path + "/template-" + os_type
+        print("Executing: " + command)
+        result = invoke.run(command, hide=True)
+
     with open("/tmp/" + image_zip_name, "wb") as handle:
         try:
-            for data in tqdm(image_download_stream.iter_content(chunk_size=chunk_size), desc="Downloading " + os_type + "...", colour="green", total=image_size, initial=0, unit="b", unit_divisor=1024, unit_scale=True):
+            for data in tqdm(image_download_stream.iter_content(chunk_size=chunk_size), desc="Downloading " + os_type + "... ", colour="green", total=image_size, initial=0, unit="b", unit_divisor=1024, unit_scale=True):
                 handle.write(data)
         except KeyboardInterrupt as e:
             print("Process was cancelled by the user (Ctrl+C)")
@@ -56,7 +74,9 @@ def download(
 
     if zipfile.is_zipfile("/tmp/" + image_zip_name):
         with zipfile.ZipFile("/tmp/" + image_zip_name, "r") as zip_ref:
-            zip_ref.extractall("/tmp/")
+            zip_ref.extractall(image_end_location)
+            os.remove("/tmp/" + image_zip_name)
+
     else:
         print("Sorry, the downloaded file is not a ZIP archive: " + "/tmp/" + image_zip_name, file=sys.stderr)
         sys.exit(1)
