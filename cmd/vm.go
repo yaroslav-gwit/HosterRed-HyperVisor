b99d2912ce6bd2b9c0053a87a418b9a8451b1224
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-10 15:44:51 +0000

    test

--- cmd/vm.go
+++ cmd/vm.go
@@ -271,7 +271,7 @@ func getVmUptimeNew(vmName string) {
 			v = strings.TrimSpace(v)
 			v = strings.Split(v, " ")[0]
 			fmt.Println(vmName, v)
-		} else if i == (len(vmsUptime)-1) && !rexMatchVmName.MatchString(v) {
+		} else if i == (len(vmsUptime) - 1) {
 			fmt.Println(vmName, "offline")
 		}
 	}
