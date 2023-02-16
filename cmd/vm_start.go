74e768f3d77e76c667e9ff9684a4b5e05361ad43
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-16 20:16:42 +0000

    test

--- cmd/vm_start.go
+++ cmd/vm_start.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
 )
 
 var (
@@ -13,8 +20,80 @@ var (
 		Long:  `Start a particular VM using it's name`,
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-			fmt.Println(args[0])
+			// cmd.Help()
+			// fmt.Println(args[0])
+			vmStart(args[0])
 		},
 	}
 )
+
+func vmStart(vmName string) error {
+	allVms := getAllVms()
+	if slices.Contains(allVms, vmName) {
+		generateBhyveStartCommand(vmName)
+	} else {
+		return errors.New("VM is not found in the system")
+	}
+
+	return nil
+}
+
+func test() {
+	for {
+		cmd := exec.Command("your-command")
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+		// Create pipes for stdout and stderr
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			fmt.Printf("Error creating stdout pipe: %s\n", err)
+			os.Exit(1)
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			fmt.Printf("Error creating stderr pipe: %s\n", err)
+			os.Exit(1)
+		}
+
+		// Start the child process
+		err = cmd.Start()
+		if err != nil {
+			fmt.Printf("Error starting command: %s\n", err)
+			os.Exit(1)
+		}
+
+		// Read from stdout and stderr
+		stdoutScanner := bufio.NewScanner(stdout)
+		stderrScanner := bufio.NewScanner(stderr)
+		go func() {
+			for stdoutScanner.Scan() {
+				fmt.Println(stdoutScanner.Text())
+			}
+		}()
+		go func() {
+			for stderrScanner.Scan() {
+				fmt.Println(stderrScanner.Text())
+			}
+		}()
+
+		// Wait for the child process to exit
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Printf("Error waiting for command: %s\n", err)
+			os.Exit(1)
+		} else {
+			fmt.Println("Process exited")
+		}
+	}
+}
+
+func generateBhyveStartCommand(vmName string) string {
+	cmd := exec.Command("ifconfig")
+	stdout, stderr := cmd.Output()
+	if stderr != nil {
+		log.Fatal("ifconfig exited with an error " + stderr.Error())
+	}
+
+	fmt.Println(string(stdout))
+	return ""
+}
