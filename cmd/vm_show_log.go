0fa6bcc4783dfc5ec612ebf67cc7b7f1a00cc178
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-19 15:09:56 +0000

    test

--- cmd/vm_show_log.go
+++ cmd/vm_show_log.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"io"
 	"os"
 	"os/exec"
 
-	"github.com/kr/pty"
 	"github.com/spf13/cobra"
 )
 
@@ -24,25 +22,30 @@ var (
 func viewLog(vmName string) {
 	vmFolder := getVmFolder(vmName)
 	tailCmd := exec.Command("micro", vmFolder+"/vm_supervisor.log")
+	// cmd := exec.Command("bash", "-i")
+	tailCmd.Stdin = os.Stdin
+	tailCmd.Stdout = os.Stdout
+	tailCmd.Stderr = os.Stderr
+	tailCmd.Run()
 	// tailCmd := exec.Command("tail", "-n", "35", "-f", vmFolder+"/vm_supervisor.log")
 
 	// Start the command with a new pseudo-terminal
-	ptmx, err := pty.Start(tailCmd)
-	if err != nil {
-		panic(err)
-	}
+	// ptmx, err := pty.Start(tailCmd)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// Forward input/output to the terminal
-	go func() {
-		_, _ = io.Copy(ptmx, os.Stdin)
-	}()
-	go func() {
-		_, _ = io.Copy(os.Stdout, ptmx)
-	}()
+	// go func() {
+	// 	_, _ = io.Copy(ptmx, os.Stdin)
+	// }()
+	// go func() {
+	// 	_, _ = io.Copy(os.Stdout, ptmx)
+	// }()
 
 	// Wait for the command to exit
-	if err := tailCmd.Wait(); err != nil {
-		_ = err
-		// fmt.Println("Command failed:", err)
-	}
+	// if err := tailCmd.Wait(); err != nil {
+	// 	_ = err
+	// fmt.Println("Command failed:", err)
+	// }
 }
