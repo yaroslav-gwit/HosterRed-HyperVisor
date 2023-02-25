2adf33ab79cfe3f45875094c7c4a03bda0cd0fbe
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-25 22:19:41 +0000

    test

--- cmd/init.go
+++ cmd/init.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os/exec"
@@ -23,6 +24,10 @@ var (
 			for _, v := range result {
 				fmt.Println(v)
 			}
+			err = applySysctls()
+			if err != nil {
+				log.Fatal(err.Error())
+			}
 		},
 	}
 )
@@ -40,19 +45,18 @@ var (
 // sysctl net.link.tap.up_on_open=1
 // 13.0-RELEASE-p11
 
-// Returns a list modules that are not yet loaded, or an error
+// Returns a list of kernel modules that are not yet loaded, or an error
 func returnMissingModules() ([]string, error) {
 	var result []string
-	stdout, stderr := exec.Command("kldstat", "-v").Output()
+	stdout, stderr := exec.Command("kldstat", "-v").CombinedOutput()
 	if stderr != nil {
-		return []string{}, stderr
+		return []string{}, errors.New("error running kldstat: " + string(stdout) + " " + stderr.Error())
 	}
 
 	reMatchKo := regexp.MustCompile(`\.ko`)
 	reSplitSpace := regexp.MustCompile(`\s+`)
-	kernelModuleList := []string{"vmm", "nmdm", "if_bridge", "pf", "pflog"}
-	kernelModuleListNoKo := []string{"if_tuntap", "if_tap"}
 
+	kernelModuleList := []string{"vmm", "nmdm", "if_bridge", "pf", "pflog"}
 	for _, v := range strings.Split(string(stdout), "\n") {
 		if reMatchKo.MatchString(v) {
 			for _, vv := range kernelModuleList {
@@ -71,6 +75,7 @@ func returnMissingModules() ([]string, error) {
 		}
 	}
 
+	kernelModuleListNoKo := []string{"if_tuntap", "if_tap"}
 	for _, v := range strings.Split(string(stdout), "\n") {
 		for _, vv := range kernelModuleListNoKo {
 			reMatchModule := regexp.MustCompile(vv)
@@ -85,5 +90,26 @@ func returnMissingModules() ([]string, error) {
 		}
 	}
 
+	stdout, stderr = exec.Command("sysctl", "net.link.tap.up_on_open").CombinedOutput()
+	if stderr != nil {
+		return []string{}, errors.New("error running sysctl: " + string(stdout) + " " + stderr.Error())
+	}
+
 	return result, nil
+}
+
+func applySysctls() error {
+	stdout, stderr := exec.Command("sysctl", "net.link.tap.up_on_open").CombinedOutput()
+	if stderr != nil {
+		return errors.New("error running sysctl: " + string(stdout) + " " + stderr.Error())
+	}
+
+	reSplitSpace := regexp.MustCompile(`\s+`)
+	for _, v := range reSplitSpace.Split(string(stdout), -1) {
+		if v == "1" {
+			fmt.Println("net.link.tap.up_on_open is loaded!")
+		}
+	}
+
+	return nil
 }
