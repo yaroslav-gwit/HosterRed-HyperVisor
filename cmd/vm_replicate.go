445c8ff4f64470ad3aa0ef2834752e8bbad1231f
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-27 15:52:46 +0000

    test

--- cmd/vm_replicate.go
+++ cmd/vm_replicate.go
@@ -164,8 +164,8 @@ func sendInitialSnapshot() {
 	var snapshotSize int
 	for _, v := range strings.Split(string(out), "\n") {
 		if reMatchSize.MatchString(v) {
-			fmt.Println(reMatchWhitespace.Split(v, -1))
-			tempInt, _ := strconv.Atoi(reMatchWhitespace.Split(v, -1)[0])
+			fmt.Println(reMatchWhitespace.Split(v, -1)[1])
+			tempInt, _ := strconv.Atoi(reMatchWhitespace.Split(v, -1)[1])
 			snapshotSize = int(tempInt)
 		}
 	}
