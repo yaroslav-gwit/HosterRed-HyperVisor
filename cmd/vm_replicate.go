c86a3761215b0e00a72888512dd14fda7000627b
Yaroslav Koisa <yaroslav@gateway-it.com>
2023-02-28 17:48:32 +0000

    test

--- cmd/vm_replicate.go
+++ cmd/vm_replicate.go
@@ -125,7 +125,7 @@ func replicateVm(vmName string, replicationEndpoint string, endpointSshPort int,
 		}
 	} else {
 		for i, v := range localVmSnaps {
-			if slices.Contains(snapsToSend, v) && i-1 != len(localVmSnaps) {
+			if slices.Contains(snapsToSend, v) && i < len(localVmSnaps) {
 				err = sendIncrementalSnapshot(vmDataset, localVmSnaps[i-1], v, replicationEndpoint, endpointSshPort, sshKeyLocation)
 				if err != nil {
 					return err
